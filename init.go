d4faac338bc339291ef5c36623b683fe93f7e8cf
810FC003 <91359472+810FC003@users.noreply.github.com>
2020-12-18 17:43:16 +0000

    New translations en.json (Thai) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- init.go
+++ init.go
@@ -4,3 +4,15 @@ package main
 import (
 	"flag"
 	"io/ioutil"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+const defaultChromedriver = "./chromedriver"
+const defaultDownDir = "."
+const driverPort = 54678
+const defaultChromeBinary = `C:\Program Files (x86)\Google\Chrome\Application\chrome.exe`
+const defaultStart = 1
+const defaultEnd = 0
+const defaultStartIter = 1
