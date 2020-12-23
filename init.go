edca8f3f1960e7919f0b9abf83a1024137332cb0
810FC003 <91359472+810FC003@users.noreply.github.com>
2020-12-23 19:17:52 +0000

    Update AliAnalysisTaskNetLambdaTrad.cxx

--- init.go
+++ init.go
@@ -30,3 +30,11 @@ func init() {
 	flag.StringVar(&argDriver, "driver", defaultChromedriver, "Path to Chromedriver binary")
 	flag.StringVar(&argDownloadDir, "o", defaultDownDir, "Where to save the reports")
 	flag.StringVar(&chromeBinary, "chrome", defaultChromeBinary, "Path to Chrome(ium) binary")
+	flag.IntVar(&argStart, "from", defaultStart, "What record to start from")
+	flag.IntVar(&argEnd, "to", defaultEnd, "What record to end with")
+	flag.IntVar(&argStartIter, "filen", defaultStartIter, "File number to start with")
+	flag.Parse()
+
+	ltempDir, err := ioutil.TempDir(os.TempDir(), "wosDownloader")
+	if err != nil {
+		logrus.Panicln(err)
