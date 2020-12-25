05d4af85b27e0ac9442bab6d3f7366e360e8236b
810FC003 <91359472+810FC003@users.noreply.github.com>
2020-12-25 10:11:37 +0000

    Remove unnecessary cast

--- logrusconf_windows.go
+++ logrusconf_windows.go
@@ -16,3 +16,10 @@ func logrusConf() {
 	//and then force logrus to emit those sequences
 	if runtime.GOOS == "windows" {
 		var originalMode uint32
+		stdout := windows.Handle(os.Stdout.Fd())
+		windows.GetConsoleMode(stdout, &originalMode)
+		windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+		logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+		//logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+	}
+}
