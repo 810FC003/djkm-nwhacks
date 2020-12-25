63865ebaf9f241f71a4355f0e70183e7f716d585
810FC003 <91359472+810FC003@users.noreply.github.com>
2020-12-25 08:33:32 +0000

    rails_apps_composer: front-end framework

--- logrusconf_windows.go
+++ logrusconf_windows.go
@@ -2,3 +2,17 @@
 // +build windows
 
 package main
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/windows"
+)
+
+func logrusConf() {
+	//if we're running on windows, enable VT sequence support
+	//and then force logrus to emit those sequences
+	if runtime.GOOS == "windows" {
+		var originalMode uint32
