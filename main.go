a380e368c7fb8efd799b2792636d31618b913621
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-02-24 22:14:31 +0000

    Added changelog file

--- main.go
+++ main.go
@@ -24,4 +24,4 @@ func main() {
 	logrus.Infof("Temporary download directory %s", tempDir)
 
 	logrus.Infoln("Starting Chrome")
-	caps := selenium.Capabilities{"brow
+	caps := selenium.Capabilities{"browserName"
