2218ddb456e5945c97378315dcf4aeb121a9f1dd
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-03-17 05:45:33 +0000

    Contact page

--- main.go
+++ main.go
@@ -29,4 +29,4 @@ func main() {
 		Path: chromeBinary,
 		Prefs: map[string]interface{}{
 			"profile.default_content_settings.popups": 0,
-			"download.default_directory":              te
+			"download.default_directory":              tempDir
