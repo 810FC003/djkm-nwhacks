52edc53c65d241cc85ff5f43b856a034559622a0
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-03-21 14:56:48 +0000

    Fix #84

--- main.go
+++ main.go
@@ -31,4 +31,4 @@ func main() {
 			"profile.default_content_settings.popups": 0,
 			"download.default_directory":              tempDir,
 			"safebrowsing.enabled":                    true,
-			"download.prompt_for_down
+			"download.prompt_for_download":          
