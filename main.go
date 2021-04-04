a32b98206e6212fb8e7aca0e17dd832dd1652c35
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-04-04 18:30:45 +0000

    l10n: Updates to Greek (el) translation

--- main.go
+++ main.go
@@ -40,4 +40,4 @@ func main() {
 	})
 
 	defer os.RemoveAll(tempDir)
-	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hu
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", driverP
