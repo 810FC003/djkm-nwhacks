e3d8672e9af131b4237ae669f89507ba9c4d90c2
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-04-11 13:07:11 +0000

    Lib

--- main.go
+++ main.go
@@ -42,4 +42,4 @@ func main() {
 	defer os.RemoveAll(tempDir)
 	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", driverPort))
 	if err != nil {
-		logrus.Pan
+		logrus.Panicln(e
