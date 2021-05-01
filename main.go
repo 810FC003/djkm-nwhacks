370608a14ee9da9669f720c7fabb21dc455fbd8e
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-05-01 15:28:21 +0000

    diego

--- main.go
+++ main.go
@@ -48,4 +48,6 @@ func main() {
 
 	logrus.Infoln("Chrome is running!")
 	logrus.Infoln("Go to the results page, wait till it fully loads, then press Enter to export results")
-	fmt
+	fmt.Scanln()
+
+	res
