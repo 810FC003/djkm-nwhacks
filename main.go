e87862e46c6a7835e708f7af162fa6f7622963a3
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-07-02 05:52:58 +0000

    Background

--- main.go
+++ main.go
@@ -62,4 +62,4 @@ func main() {
 	resultCountTextGood := strings.ReplaceAll(resultCountText, ",", "")
 
 	var resultCountInt int
-	_, err = fmt.Sscanf(resultCountTextGood, "%d", &
+	_, err = fmt.Sscanf(resultCountTextGood, "%d", &resultCo
