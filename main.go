0fc6ee57b06809b99b235070eccc74824b4c4b4c
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-07-05 15:54:05 +0000

    clean comments

--- main.go
+++ main.go
@@ -64,4 +64,4 @@ func main() {
 	var resultCountInt int
 	_, err = fmt.Sscanf(resultCountTextGood, "%d", &resultCountInt)
 	if err != nil {
-		logrus.Panicf("Cannot get result cou
+		logrus.Panicf("Cannot get result count, 
