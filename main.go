0675ff86733f05c5c46c81c45ac95e14bf3609aa
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-11-09 13:22:14 +0000

    This is a move refactor.

--- main.go
+++ main.go
@@ -97,4 +97,5 @@ func main() {
 
 		_, err = wd.FindElement(selenium.ByXPATH, `//*[@id="exportTypeName"]`)
 		if err == nil {
-			mustClickByXPATH(wd, `//*[@id="expor
+			mustClickByXPATH(wd, `//*[@id="exportTypeName"]`)
+		} e
