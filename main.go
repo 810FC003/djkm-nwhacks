5acb2c82c1a650c69fb9640bc784004b29586b7e
810FC003 <91359472+810FC003@users.noreply.github.com>
2021-12-05 07:08:45 +0000

    Removed old images

--- main.go
+++ main.go
@@ -104,4 +104,4 @@ func main() {
 
 		time.Sleep(500 * time.Millisecond)
 
-		_, err = wd.FindElement(selenium.ByXPATH, `/html/body/div[1]/div[26]/div[2]/div/div/div/div[2]/div[3]/div[3]/div[2]/div[1]/ul/
+		_, err = wd.FindElement(selenium.ByXPATH, `/html/body/div[1]/div[26]/div[2]/div/div/div/div[2]/div[3]/div[3]/div[2]/div[1]/ul/li/span/ul/
