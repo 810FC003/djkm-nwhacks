6bc4675664cfed24e54243b95c94ab27f4773147
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-03-15 21:51:42 +0000

    Create phpunit.xml

--- main.go
+++ main.go
@@ -121,4 +121,4 @@ func main() {
 		time.Sleep(30 * time.Millisecond)
 
 		mustClickByXPATH(wd, `/html/body/div[11]/div[2]/form/div[2]/div[2]/div/span/span[1]/span/span[2]`)
-		time.Sleep(30
+		time.Sleep(30 * time.
