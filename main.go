bc8e8f3e8d09d1921666a816dca50895c2117ea7
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-05-07 23:05:16 +0000

    Ugh.

--- main.go
+++ main.go
@@ -127,4 +127,5 @@ func main() {
 		mustClickByXPATH(wd, `/html/body/div[11]/div[2]/form/div[2]/div[2]/div/span/span[1]/span/span[2]`)
 		time.Sleep(30 * time.Millisecond)
 
-		mustClickByXPATH(wd, `/html/body/div[11]/div[2]/form/div[3]/div/div/div/span/span[1]/span/s
+		mustClickByXPATH(wd, `/html/body/div[11]/div[2]/form/div[3]/div/div/div/span/span[1]/span/span[2]`)
+		
