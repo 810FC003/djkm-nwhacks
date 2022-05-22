e1fba4c7d8e53586acce86fc33f7e341b1e5aa1f
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-05-22 17:16:47 +0000

    Markdown support

--- main.go
+++ main.go
@@ -130,4 +130,4 @@ func main() {
 		mustClickByXPATH(wd, `/html/body/div[11]/div[2]/form/div[3]/div/div/div/span/span[1]/span/span[2]`)
 		time.Sleep(30 * time.Millisecond)
 		mustClickByXPATH(wd, `//*[@id="saveOptions"]/option[4]`)
-		time.Sleep(3
+		time.Sleep(30 * ti
