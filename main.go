f2dd9450e56649b9e30be8c03b7cb4ea9f5e7150
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-07-30 22:30:32 +0000

    updating libs

--- main.go
+++ main.go
@@ -141,4 +141,4 @@ func main() {
 			logrus.Panic(err)
 		}
 
-		err = portableMoveFile(tempDir+"/"+fname, argDownloadDir+"/"+fmt.Sprintf("%s_%d", "WOS_EXP", iteratio
+		err = portableMoveFile(tempDir+"/"+fname, argDownloadDir+"/"+fmt.Sprintf("%s_%d", "WOS_EXP", iterationN)+"_"
