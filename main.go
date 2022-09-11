0cf8be3cb4b7defa5088d38422acc236a5781e03
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-09-11 15:34:46 +0000

    removed changelog

--- main.go
+++ main.go
@@ -149,4 +149,4 @@ func main() {
 		time.Sleep(700 * time.Millisecond)
 		mustClickByXPATH(wd, `//*[@id="page"]/div[11]/div[2]/form/div[2]/a`)
 
-		logrus.Infof("File saved to %s\n", argDownloadDir+"/"+fmt.Sprintf("%s_
+		logrus.Infof("File saved to %s\n", argDownloadDir+"/"+fmt.Sprintf("%s_%d", "WO
