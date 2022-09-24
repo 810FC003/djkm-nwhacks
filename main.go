b67f18e7e0fe456f39bb44cf34710f8d5b71a2fc
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-09-24 15:02:12 +0000

    docstring

--- main.go
+++ main.go
@@ -152,4 +152,5 @@ func main() {
 		logrus.Infof("File saved to %s\n", argDownloadDir+"/"+fmt.Sprintf("%s_%d", "WOS_EXP", iterationN)+"_"+fname)
 
 		iterationN++
-		lastDownloaded = lastT
+		lastDownloaded = lastTD
+		re
