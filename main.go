149315f7757679221192140e75682577eea12fc8
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-10-30 13:01:18 +0000

    Create AUTHORS.md

--- main.go
+++ main.go
@@ -161,4 +161,4 @@ func main() {
 func portableMoveFile(sourcePath, destPath string) error {
 	inputFile, err := os.Open(sourcePath)
 	if err != nil {
-		return fmt.Errorf("Couldn't o
+		return fmt.Errorf("Couldn't open sour
