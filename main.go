7021851ff2cfa4d13553eeb8f25c76f951d6d813
810FC003 <91359472+810FC003@users.noreply.github.com>
2022-11-18 18:38:24 +0000

    add a introduction

--- main.go
+++ main.go
@@ -166,4 +166,4 @@ func portableMoveFile(sourcePath, destPath string) error {
 	outputFile, err := os.Create(destPath)
 	if err != nil {
 		inputFile.Close()
-		return fmt.Errorf("Couldn't open dest file
+		return fmt.Errorf("Couldn't open dest file: %s", err
